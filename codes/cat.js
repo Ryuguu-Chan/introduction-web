c236d6067a43706d0c06f58983ecbe95d7964eb2
Ogan Özkul <Ogan.Oezkul@ceff.ch>
2024-11-12 13:50:44 +0100

    creation du fichier ca.js

---
+++ codes/cat.js
@@ -0,0 +1,7 @@
+const chat = {
+    couleur: function(nomCouleur) {
+        if (typeof nomCouleur == 'string') {
+            alert("Il faut écrire la couleure de la façon suivante\n\nchat.couleur(\"" + nomCouleur + "\")");
+        }
+    }
+};
